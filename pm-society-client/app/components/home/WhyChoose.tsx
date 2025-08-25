fdd3bb43691f52e31898ec845c0bcf7da47fa5bd
iamdemon <emonhowlader.r@gmail.com>
2025-08-25 18:41:16 +0600

    framer fixed issues

--- pm-society-client/app/components/home/WhyChoose.tsx
+++ pm-society-client/app/components/home/WhyChoose.tsx
@@ -23,12 +23,12 @@ const containerVariants = {
 
 const itemVariants = {
   hidden: { opacity: 0, y: 20 },
-  visible: { opacity: 1, y: 0, transition: { duration: 0.5, ease: [0.42, 0, 0.58, 1] } }, // cubic-bezier
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5, ease: [0.42, 0, 0.58, 1] as [number, number, number, number] } }, // cubic-bezier
 };
 
 const videoVariants = {
   hidden: { opacity: 0, scale: 0.95 },
-  visible: { opacity: 1, scale: 1, transition: { duration: 0.8, ease: [0.42, 0, 0.58, 1] } },
+  visible: { opacity: 1, scale: 1, transition: { duration: 0.8, ease: [0.42, 0, 0.58, 1]  as [number, number, number, number]} },
 };
 
 const WhyChoose: React.FC = () => {
