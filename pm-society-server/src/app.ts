5db6ccf667c51979a81e98358950c4602f385726
iamdemon <emonhowlader.r@gmail.com>
2025-08-30 02:35:31 +0600

    cors error

--- pm-society-server/src/app.ts
+++ pm-society-server/src/app.ts
@@ -8,27 +8,32 @@ import notFound from "./app/middlewares/notFound";
 const app: Application = express();
 app.use(express.json());
 
-const allowedOrigins = [
-  'https://thepmsociety.com',
-  'https://www.thepmsociety.com',
-  'https://pm-socity.vercel.app',
-  'https://pm-society.vercel.app',
-  'http://localhost:3000',
-];
+// const allowedOrigins = [
+//   'https://thepmsociety.com',
+//   'https://www.thepmsociety.com',
+//   'https://pm-socity.vercel.app',
+//   'https://pm-society.vercel.app',
+//   'http://localhost:3000',
+// ];
 
-const corsOptions = {
-  origin: (origin: string | undefined, callback: (arg0: Error | null, arg1: boolean | undefined) => void) => {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'), false);
-    }
-  },
+// const corsOptions = {
+//   origin: (origin: string | undefined, callback: (arg0: Error | null, arg1: boolean | undefined) => void) => {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'), false);
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+// };
+
+// app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-};
-
-app.use(cors(corsOptions));
+}));
 // app.use(
 //   cors({
 //     origin: ["https://thepmsociety.com","https://www.thepmsociety.com","https://pm-socity.vercel.app","http://localhost:3000",  ],
